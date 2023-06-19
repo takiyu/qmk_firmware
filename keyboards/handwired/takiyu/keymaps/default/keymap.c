857091ac2ca59b3ac11679955d8595615a9c0037
takiyu <takiyu1025.txu.development@gmail.com>
2023-06-19 15:42:21 +0000

    Update keymap

--- keyboards/handwired/takiyu/keymaps/default/keymap.c
+++ keyboards/handwired/takiyu/keymaps/default/keymap.c
@@ -11,45 +11,46 @@ enum custom_layer {
 };
 
 #define _SPACE LT(_FUN, KC_SPC)
+#define ___NG___ XXXXXXX
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_HOM] = LAYOUT_5x8(
         TO(_HOM), JP_ZKHK, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   ,    KC_7   , KC_8   , KC_9   , KC_0   , JP_MINS, JP_CIRC, JP_YEN , KC_BSPC,
-        TO(_FUN), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , _______,    KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , JP_AT  , JP_LBRC, KC_ENT ,
-        TO(_1LA), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , _______,    KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, JP_COLN, JP_RBRC, _______,
+        TO(_FUN), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,___NG___,    KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , JP_AT  , JP_LBRC, KC_ENT ,
+        TO(_1LA), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,___NG___,    KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, JP_COLN, JP_RBRC,___NG___,
         TO(_2LA),MO(_FUN), KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,    KC_B   , KC_N   , KC_M   , JP_COMM, JP_DOT , JP_SLSH, JP_BSLS, KC_LSFT,
-        TO(_3LA), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, _SPACE , _______,    _SPACE , KC_ESC , KC_RALT, KC_MENU, KC_RCTL, KC_WBAK, KC_WFWD, KC_PSCR
+        TO(_3LA), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, _SPACE ,___NG___,    _SPACE , KC_ESC , KC_RALT, KC_MENU, KC_RCTL, KC_WBAK, KC_WFWD, KC_PSCR
     ),
 
     [_FUN] = LAYOUT_5x8(
-        TO(_HOM), KC_ESC ,  KC_F1 , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,    KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , JP_YEN , KC_BSPC,
-        TO(_FUN), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , _______,    KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , JP_AT  , JP_LBRC, KC_ENT ,
-        TO(_1LA), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , _______,    KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, JP_COLN, JP_RBRC, _______,
-        TO(_2LA),MO(_FUN), KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,    KC_B   , KC_N   , KC_M   , JP_COMM, JP_DOT , JP_SLSH, JP_BSLS, KC_LSFT,
-        TO(_3LA), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, _SPACE , _______,    _SPACE , KC_ESC , KC_RALT, KC_MENU, KC_RCTL, KC_WBAK, KC_WFWD, KC_PSCR
+        TO(_HOM), KC_ESC , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,    KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , XXXXXXX, KC_DEL ,
+        TO(_FUN), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,___NG___,    KC_Y   , KC_U   , KC_INS , KC_O   , KC_P   , JP_AT  , KC_PGUP, KC_ENT ,
+        TO(_1LA), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,___NG___,    KC_LEFT, KC_DOWN, KC_UP  ,KC_RIGHT, XXXXXXX, XXXXXXX, KC_PGDN,___NG___,
+        TO(_2LA),MO(_FUN), KC_LSFT, XXXXXXX, KC_DEL , KC_C   , KC_V   , KC_B   ,    KC_B   , KC_N   , KC_M   , JP_COMM, JP_DOT , JP_SLSH, JP_BSLS, KC_LSFT,
+        TO(_3LA), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, _SPACE ,___NG___,    _SPACE , KC_ESC , KC_RALT, KC_MENU, KC_RCTL, KC_WBAK, KC_WFWD, KC_PSCR
     ),
 
     [_1LA] = LAYOUT_5x8(
         TO(_HOM), JP_ZKHK, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   ,    KC_7   , KC_8   , KC_9   , KC_0   , JP_MINS, JP_CIRC, JP_YEN , KC_BSPC,
-        TO(_FUN), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , _______,    KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , JP_AT  , JP_LBRC, KC_ENT ,
-        TO(_1LA), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , _______,    KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, JP_COLN, JP_RBRC, _______,
+        TO(_FUN), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,___NG___,    KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , JP_AT  , JP_LBRC, KC_ENT ,
+        TO(_1LA), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,___NG___,    KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, JP_COLN, JP_RBRC,___NG___,
         TO(_2LA),MO(_FUN), KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,    KC_B   , KC_N   , KC_M   , JP_COMM, JP_DOT , JP_SLSH, JP_BSLS, KC_LSFT,
-        TO(_3LA), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, _SPACE , _______,    _SPACE , KC_ESC , KC_RALT, KC_MENU, KC_RCTL, KC_WBAK, KC_WFWD, KC_PSCR
+        TO(_3LA), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, _SPACE ,___NG___,    _SPACE , KC_ESC , KC_RALT, KC_MENU, KC_RCTL, KC_WBAK, KC_WFWD, KC_PSCR
     ),
 
     [_2LA] = LAYOUT_5x8(
         TO(_HOM), JP_ZKHK, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   ,    KC_7   , KC_8   , KC_9   , KC_0   , JP_MINS, JP_CIRC, JP_YEN , KC_BSPC,
-        TO(_FUN), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , _______,    KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , JP_AT  , JP_LBRC, KC_ENT ,
-        TO(_1LA), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , _______,    KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, JP_COLN, JP_RBRC, _______,
+        TO(_FUN), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,___NG___,    KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , JP_AT  , JP_LBRC, KC_ENT ,
+        TO(_1LA), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,___NG___,    KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, JP_COLN, JP_RBRC,___NG___,
         TO(_2LA),MO(_FUN), KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,    KC_B   , KC_N   , KC_M   , JP_COMM, JP_DOT , JP_SLSH, JP_BSLS, KC_LSFT,
-        TO(_3LA), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, _SPACE , _______,    _SPACE , KC_ESC , KC_RALT, KC_MENU, KC_RCTL, KC_WBAK, KC_WFWD, KC_PSCR
+        TO(_3LA), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, _SPACE ,___NG___,    _SPACE , KC_ESC , KC_RALT, KC_MENU, KC_RCTL, KC_WBAK, KC_WFWD, KC_PSCR
     ),
 
     [_3LA] = LAYOUT_5x8(
         TO(_HOM), JP_ZKHK, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   ,    KC_7   , KC_8   , KC_9   , KC_0   , JP_MINS, JP_CIRC, JP_YEN , KC_BSPC,
-        TO(_FUN), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   , _______,    KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , JP_AT  , JP_LBRC, KC_ENT ,
-        TO(_1LA), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   , _______,    KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, JP_COLN, JP_RBRC, _______,
+        TO(_FUN), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,___NG___,    KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , JP_AT  , JP_LBRC, KC_ENT ,
+        TO(_1LA), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,___NG___,    KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, JP_COLN, JP_RBRC,___NG___,
         TO(_2LA),MO(_FUN), KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,    KC_B   , KC_N   , KC_M   , JP_COMM, JP_DOT , JP_SLSH, JP_BSLS, KC_LSFT,
-        TO(_3LA), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, _SPACE , _______,    _SPACE , KC_ESC , KC_RALT, KC_MENU, KC_RCTL, KC_WBAK, KC_WFWD, KC_PSCR
+        TO(_3LA), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, _SPACE ,___NG___,    _SPACE , KC_ESC , KC_RALT, KC_MENU, KC_RCTL, KC_WBAK, KC_WFWD, KC_PSCR
     )
 };
