f09ee314a20cf04fee239d0d5a51b5afb46d1357
takiyu <takiyu1025.txu.development@gmail.com>
2023-06-19 14:05:30 +0000

    Change to 5 layers

--- keyboards/handwired/takiyu/keymaps/default/keymap.c
+++ keyboards/handwired/takiyu/keymaps/default/keymap.c
@@ -1,16 +1,15 @@
 #include QMK_KEYBOARD_H
 
 enum custom_layer {
-    _QWERTY,
-    _LOWER,
-    _RAISE,
+    _HOME,
+    _FUNC,
+    _1,
+    _2,
+    _3,
 };
 
-#define RAISE MO(_RAISE)
-#define LOWER MO(_LOWER)
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_QWERTY] = LAYOUT_5x8(
+    [_HOME] = LAYOUT_5x8(
         KC_ESC , KC_ESC , KC_F1   , KC_F2  , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,        KC_F7 , KC_F8  , KC_F9  , KC_F10, KC_F11, KC_F12   , _______, KC_ESC ,
         _______, _______, KC_GRV  , KC_1   , KC_2  , KC_3  , KC_4  , KC_5  ,        KC_6  , KC_7   , KC_8   , KC_9  , KC_0  , KC_LBRC  , KC_RBRC, _______,
         _______, _______, KC_TAB  , KC_Q   , KC_W  , KC_E  , KC_R  , KC_T  ,        KC_Y  , KC_U   , KC_I   , KC_O  , KC_P  , KC_QUOT  , KC_BSLS, _______,
@@ -18,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, KC_LCTL , KC_Z   , KC_X  , KC_C  , KC_V  , KC_B  ,        KC_N  , KC_M   , KC_COMM, KC_DOT,KC_SLSH, KC_LCTL  , _______, _______
     ),
 
-    [_LOWER] = LAYOUT_5x8(
+    [_FUNC] = LAYOUT_5x8(
         KC_ESC , KC_ESC , KC_F1   , KC_F2  , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,        KC_F7 , KC_F8  , KC_F9  , KC_F10, KC_F11, KC_F12   , _______, KC_ESC ,
         _______, _______, KC_GRV  , KC_1   , KC_2  , KC_3  , KC_4  , KC_5  ,        KC_6  , KC_7   , KC_8   , KC_9  , KC_0  , KC_LBRC  , KC_RBRC, _______,
         _______, _______, KC_TAB  , KC_Q   , KC_W  , KC_E  , KC_R  , KC_T  ,        KC_Y  , KC_U   , KC_I   , KC_O  , KC_P  , KC_QUOT  , KC_BSLS, _______,
@@ -26,7 +25,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, KC_LCTL , KC_Z   , KC_X  , KC_C  , KC_V  , KC_B  ,        KC_N  , KC_M   , KC_COMM, KC_DOT,KC_SLSH, KC_LCTL  , _______, _______
     ),
 
-    [_RAISE] = LAYOUT_5x8(
+    [_1] = LAYOUT_5x8(
+        KC_ESC , KC_ESC , KC_F1   , KC_F2  , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,        KC_F7 , KC_F8  , KC_F9  , KC_F10, KC_F11, KC_F12   , _______, KC_ESC ,
+        _______, _______, KC_GRV  , KC_1   , KC_2  , KC_3  , KC_4  , KC_5  ,        KC_6  , KC_7   , KC_8   , KC_9  , KC_0  , KC_LBRC  , KC_RBRC, _______,
+        _______, _______, KC_TAB  , KC_Q   , KC_W  , KC_E  , KC_R  , KC_T  ,        KC_Y  , KC_U   , KC_I   , KC_O  , KC_P  , KC_QUOT  , KC_BSLS, _______,
+        _______, _______, KC_LSFT , KC_A   , KC_S  , KC_D  , KC_F  , KC_G  ,        KC_H  , KC_J   , KC_K   , KC_L  ,KC_SCLN, KC_LSFT  , _______, _______,
+        _______, _______, KC_LCTL , KC_Z   , KC_X  , KC_C  , KC_V  , KC_B  ,        KC_N  , KC_M   , KC_COMM, KC_DOT,KC_SLSH, KC_LCTL  , _______, _______
+    ),
+
+    [_2] = LAYOUT_5x8(
+        KC_ESC , KC_ESC , KC_F1   , KC_F2  , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,        KC_F7 , KC_F8  , KC_F9  , KC_F10, KC_F11, KC_F12   , _______, KC_ESC ,
+        _______, _______, KC_GRV  , KC_1   , KC_2  , KC_3  , KC_4  , KC_5  ,        KC_6  , KC_7   , KC_8   , KC_9  , KC_0  , KC_LBRC  , KC_RBRC, _______,
+        _______, _______, KC_TAB  , KC_Q   , KC_W  , KC_E  , KC_R  , KC_T  ,        KC_Y  , KC_U   , KC_I   , KC_O  , KC_P  , KC_QUOT  , KC_BSLS, _______,
+        _______, _______, KC_LSFT , KC_A   , KC_S  , KC_D  , KC_F  , KC_G  ,        KC_H  , KC_J   , KC_K   , KC_L  ,KC_SCLN, KC_LSFT  , _______, _______,
+        _______, _______, KC_LCTL , KC_Z   , KC_X  , KC_C  , KC_V  , KC_B  ,        KC_N  , KC_M   , KC_COMM, KC_DOT,KC_SLSH, KC_LCTL  , _______, _______
+    ),
+
+    [_3] = LAYOUT_5x8(
         KC_ESC , KC_ESC , KC_F1   , KC_F2  , KC_F3 , KC_F4 , KC_F5 , KC_F6 ,        KC_F7 , KC_F8  , KC_F9  , KC_F10, KC_F11, KC_F12   , _______, KC_ESC ,
         _______, _______, KC_GRV  , KC_1   , KC_2  , KC_3  , KC_4  , KC_5  ,        KC_6  , KC_7   , KC_8   , KC_9  , KC_0  , KC_LBRC  , KC_RBRC, _______,
         _______, _______, KC_TAB  , KC_Q   , KC_W  , KC_E  , KC_R  , KC_T  ,        KC_Y  , KC_U   , KC_I   , KC_O  , KC_P  , KC_QUOT  , KC_BSLS, _______,
