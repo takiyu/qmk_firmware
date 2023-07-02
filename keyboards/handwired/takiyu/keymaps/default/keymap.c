b91831621cee91bcce77768e9a39a0fca6336079
takiyu <takiyu1025.txu.development@gmail.com>
2023-07-02 00:58:49 +0000

    Use dynamic macro

--- keyboards/handwired/takiyu/keymaps/default/keymap.c
+++ keyboards/handwired/takiyu/keymaps/default/keymap.c
@@ -25,16 +25,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TO(_HOM), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,___NG___,    KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , JP_AT  , JP_LBRC, KC_ENT ,
         TO(_1FN), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,___NG___,    KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, JP_COLN, JP_RBRC,___NG___,
         TO(_2VI),MO(_1FN), KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,    KC_B   , KC_N   , KC_M   , JP_COMM, JP_DOT , JP_SLSH, JP_BSLS, KC_RSFT,
-        TO(_3DE), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, TK_SPC1,___NG___,    TK_SPC1, KC_ESC , KC_RALT, KC_MENU, KC_RCTL, KC_WBAK, KC_WFWD, KC_PSCR
+        TO(_3DE), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, TK_SPC1,___NG___,    TK_SPC1, KC_ESC , KC_RALT, KC_MENU, KC_RCTL, KC_LEFT,KC_RIGHT, KC_PSCR
     ),
 
     // Function Layer
     [_1FN] = LAYOUT_5x8(
         KC_ESC,   _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,    KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , XXXXXXX, KC_DEL ,
-        TO(_HOM), _______, _______, _______, _______, KC_F5  , _______,___NG___,    _______, _______, KC_INS , _______, _______, _______, KC_PGUP, _______,
-        TO(_1FN), _______, _______, _______, _______, _______, _______,___NG___,    KC_LEFT, KC_DOWN, KC_UP  ,KC_RIGHT, _______, _______, KC_PGDN,___NG___,
+        TO(_HOM), _______, DM_REC1, _______, _______, KC_F5  , _______,___NG___,    _______, _______, KC_INS , _______, _______, DM_PLY1, KC_PGUP, _______,
+        TO(_1FN), _______, DM_RSTP, _______, _______, _______, _______,___NG___,    KC_LEFT, KC_DOWN, KC_UP  ,KC_RIGHT, _______, _______, KC_PGDN,___NG___,
         TO(_2VI),MO(_1FN), _______, _______, KC_DEL , _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
-        TO(_3DE), _______, _______, _______, _______, _______, TK_SPC2,___NG___,    TK_SPC2, _______, _______, _______, _______, _______, _______, _______
+        TO(_3DE), _______, _______, _______, _______, _______, TK_SPC2,___NG___,    TK_SPC2, _______, _______, _______, _______, KC_WBAK, KC_WFWD, _______
     ),
 
     // Vim-like Layer
@@ -59,13 +59,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // Key overrides
 const key_override_t override_ctrl_del = ko_make_basic(MOD_MASK_CTRL, KC_BSPC, KC_DEL);
+const key_override_t override_shif_del = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
 const key_override_t override_ctrl_h = ko_make_basic(MOD_MASK_CTRL, KC_LEFT, KC_HOME);
-const key_override_t override_ctrl_l = ko_make_basic(MOD_MASK_CTRL, KC_RIGHT, KC_END);
 const key_override_t override_ctrl_j = ko_make_basic(MOD_MASK_CTRL, KC_DOWN, KC_PGDN);
 const key_override_t override_ctrl_k = ko_make_basic(MOD_MASK_CTRL, KC_UP, KC_PGUP);
+const key_override_t override_ctrl_l = ko_make_basic(MOD_MASK_CTRL, KC_RIGHT, KC_END);
+const key_override_t override_shif_h = ko_make_basic(MOD_MASK_SHIFT, KC_LEFT, KC_HOME);
+const key_override_t override_shif_j = ko_make_basic(MOD_MASK_SHIFT, KC_DOWN, KC_PGDN);
+const key_override_t override_shif_k = ko_make_basic(MOD_MASK_SHIFT, KC_UP, KC_PGUP);
+const key_override_t override_shif_l = ko_make_basic(MOD_MASK_SHIFT, KC_RIGHT, KC_END);
 
 const key_override_t **key_overrides = (const key_override_t *[]){
-    &override_ctrl_del,
+    &override_ctrl_del, &override_shif_del,
     &override_ctrl_h, &override_ctrl_l, &override_ctrl_j, &override_ctrl_k,
+    &override_shif_h, &override_shif_l, &override_shif_j, &override_shif_k,
     NULL // End of array
 };
