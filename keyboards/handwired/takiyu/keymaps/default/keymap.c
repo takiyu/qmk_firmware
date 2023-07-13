e660d38baf030f8baeddf1db26e46ce676f214ce
takiyu <takiyu1025.txu.development@gmail.com>
2023-07-13 15:19:51 +0000

    Rename layer

--- keyboards/handwired/takiyu/keymaps/default/keymap.c
+++ keyboards/handwired/takiyu/keymaps/default/keymap.c
@@ -5,7 +5,7 @@
 enum custom_layer {
     _HOM,
     _1FN,
-    _2VI,
+    _2MO,
     _3DE,
 };
 
@@ -14,7 +14,7 @@ enum custom_layer {
 
 #define ___NG___ XXXXXXX
 #define TK_SPC1  LT(_1FN, KC_SPC)
-#define TK_SPC2  LT(_2VI, KC_SPC)
+#define TK_SPC2  LT(_2MO, KC_SPC)
 #define TK_TAB3  LCTL(KC_TAB)
 #define TK_TAB2  LSFT(LCTL(KC_TAB))
 #define TK_PSCR  LALT(KC_PSCR)
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   JP_ZKHK, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   ,    KC_7   , KC_8   , KC_9   , KC_0   , JP_MINS, JP_CIRC, JP_YEN , KC_BSPC,
         TO(_HOM), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,___NG___,    KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , JP_AT  , JP_LBRC, KC_ENT ,
         TO(_1FN), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,___NG___,    KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, JP_COLN, JP_RBRC,___NG___,
-        TO(_2VI), KC_LSFT, KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,    KC_B   , KC_N   , KC_M   , JP_COMM, JP_DOT , JP_SLSH, JP_BSLS, KC_RSFT,
+        TO(_2MO), KC_LSFT, KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,    KC_B   , KC_N   , KC_M   , JP_COMM, JP_DOT , JP_SLSH, JP_BSLS, KC_RSFT,
         TO(_3DE), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, TK_SPC1,___NG___,    TK_SPC1, KC_ESC , KC_RALT, KC_MENU, KC_RCTL, KC_LEFT,KC_RIGHT, KC_PSCR
     ),
 
@@ -34,16 +34,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,    KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , XXXXXXX, KC_DEL ,
         TO(_HOM), _______, DM_REC1, _______, _______, KC_WFWD, _______,___NG___,    _______, KC_WBAK, KC_INS , _______, _______, DM_PLY1, KC_PGUP, _______,
         TO(_1FN), _______, DM_RSTP, _______, _______, _______, _______,___NG___,    KC_LEFT, KC_DOWN, KC_UP  ,KC_RIGHT, _______, _______, KC_PGDN,___NG___,
-        TO(_2VI), _______, _______, _______, KC_DEL , _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
+        TO(_2MO), _______, _______, _______, KC_DEL , _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
         TO(_3DE), _______, _______, _______, _______, _______, TK_SPC2,___NG___,    TK_SPC2, _______, _______, _______, _______, KC_WBAK, KC_WFWD, TK_PSCR
     ),
 
-    // Vim-like Layer
-    [_2VI] = LAYOUT_5x8(
+    // Mouse Layer
+    [_2MO] = LAYOUT_5x8(
         KC_ESC,   _______, _______, TK_TAB2, TK_TAB3, _______, _______, _______,    _______, _______, KC_HOME, KC_END , _______, _______, _______, _______,
         TO(_HOM), _______, _______, _______, _______, _______, _______,___NG___,    _______, _______, _______, _______, _______, _______, _______, _______,
-        TO(_1FN), _______, _______, _______, KC_PGDN, _______, _______,___NG___,    _______, _______, _______, _______, _______, _______, _______,___NG___,
-        TO(_2VI), _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
+        TO(_1FN), _______, _______, _______, KC_PGDN, _______, _______,___NG___,    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, _______,___NG___,
+        TO(_2MO), _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
         TO(_3DE), _______, _______, _______, _______, _______, TK_SPC2,___NG___,    TK_SPC2, _______, _______, _______, _______, _______, _______, _______
     ),
 
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
         TO(_HOM), _______, QK_BOOT, _______, _______, _______, _______,___NG___,    _______, _______, _______, _______, QK_BOOT, _______, _______, _______,
         TO(_1FN), _______, _______, _______, _______, _______, _______,___NG___,    _______, _______, _______, _______, _______, _______, _______,___NG___,
-        TO(_2VI), _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
+        TO(_2MO), _______, _______, _______, _______, _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______, _______,
         TO(_3DE), _______, _______, _______, _______, _______, TK_SPC2,___NG___,    TK_SPC2, _______, _______, _______, _______, _______, _______, _______
     )
 };
