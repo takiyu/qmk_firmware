9a6588288a613c93eb8cd1e62142f9aa85d64780
takiyu <takiyu1025.txu.development@gmail.com>
2023-07-17 14:06:17 +0000

    Replace menu with app

--- keyboards/handwired/takiyu/keymaps/default/keymap.c
+++ keyboards/handwired/takiyu/keymaps/default/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TO(_HOM), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,___NG___,    KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , JP_AT  , JP_LBRC, KC_ENT ,
         TO(_1FN), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,___NG___,    KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, JP_COLN, JP_RBRC,___NG___,
         TO(_2MO), KC_LSFT, KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,    KC_B   , KC_N   , KC_M   , JP_COMM, JP_DOT , JP_SLSH, JP_BSLS, KC_RSFT,
-        TO(_3DE), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, TK_SPC1,___NG___,    TK_SPC2, KC_ESC , KC_RALT, KC_MENU, KC_RCTL, KC_LEFT,KC_RIGHT, KC_PSCR
+        TO(_3DE), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, TK_SPC1,___NG___,    TK_SPC2, KC_ESC , KC_RALT, KC_APP , KC_RCTL, KC_LEFT,KC_RIGHT, KC_PSCR
     ),
 
     // Function Layer
