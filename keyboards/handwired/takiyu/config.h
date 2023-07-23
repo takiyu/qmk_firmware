008f7bbed5ce7ffd87e1add9175a47f2a9a85523
takiyu <takiyu1025.txu.development@gmail.com>
2023-07-23 18:12:24 +0000

    Change mouse speed

--- keyboards/handwired/takiyu/config.h
+++ keyboards/handwired/takiyu/config.h
@@ -3,7 +3,7 @@
 /* mouse config */
 #define MOUSEKEY_INTERVAL    20
 #define MOUSEKEY_DELAY       0
-#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_TIME_TO_MAX 30
 #define MOUSEKEY_MAX_SPEED   7
 #define MOUSEKEY_WHEEL_DELAY 0
 
