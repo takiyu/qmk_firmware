b173a17a5c6918fe3034474e73436650ceea43ed
takiyu <takiyu1025.txu.development@gmail.com>
2023-07-25 12:46:28 +0000

    Disable overlapped alt-num 9,0

--- keyboards/handwired/takiyu/keymaps/default/keymap.c
+++ keyboards/handwired/takiyu/keymaps/default/keymap.c
@@ -75,8 +75,8 @@ const key_override_t override_alt_4 = ko_make_basic(MOD_MASK_ALT, KC_4, JP_DLR);
 const key_override_t override_alt_5 = ko_make_basic(MOD_MASK_ALT, KC_5, JP_PERC);
 const key_override_t override_alt_6 = ko_make_basic(MOD_MASK_ALT, KC_6, JP_AMPR);
 const key_override_t override_alt_7 = ko_make_basic(MOD_MASK_ALT, KC_7, JP_QUOT);
-const key_override_t override_alt_8 = ko_make_basic(MOD_MASK_ALT, KC_8, JP_LPRN);
-const key_override_t override_alt_9 = ko_make_basic(MOD_MASK_ALT, KC_9, JP_RPRN);
+// const key_override_t override_alt_8 = ko_make_basic(MOD_MASK_ALT, KC_8, JP_LPRN);
+// const key_override_t override_alt_9 = ko_make_basic(MOD_MASK_ALT, KC_9, JP_RPRN);
 const key_override_t override_alt_10 = ko_make_basic(MOD_MASK_ALT, KC_0, JP_TILD);
 const key_override_t override_alt_11 = ko_make_basic(MOD_MASK_ALT, JP_MINS, JP_EQL);
 const key_override_t override_alt_12 = ko_make_basic(MOD_MASK_ALT, JP_CIRC, JP_TILD);
@@ -94,8 +94,9 @@ const key_override_t override_shift_bs = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, 
 const key_override_t **key_overrides = (const key_override_t *[]){
     &override_shif_space,
     &override_alt_1, &override_alt_2, &override_alt_3, &override_alt_4,
-    &override_alt_5, &override_alt_6, &override_alt_7, &override_alt_8,
-    &override_alt_9, &override_alt_10, &override_alt_11, &override_alt_12,
+    &override_alt_5, &override_alt_6, &override_alt_7,
+    // &override_alt_8, &override_alt_9,
+    &override_alt_10, &override_alt_11, &override_alt_12,
     &override_alt_13, &override_alt_bs,
 #if USE_OVERRIDE_SHIFT_CTRL_BS
     &override_ctrl_bs, &override_shift_bs,
