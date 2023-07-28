31ba8b668b6ce2450898bbb0f5ae5998d1bf186b
takiyu <takiyu1025.txu.development@gmail.com>
2023-07-28 11:43:02 +0000

    Enable auto correct and arrange configs to make rom size smaller.

--- keyboards/handwired/takiyu/rules.mk
+++ keyboards/handwired/takiyu/rules.mk
@@ -1,19 +1,28 @@
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
+
+# Essential
 SPLIT_KEYBOARD = yes
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+LTO_ENABLE = yes            # Link Time Optimization
+
+# Features
+MOUSEKEY_ENABLE = yes
 KEY_OVERRIDE_ENABLE = yes
 DYNAMIC_MACRO_ENABLE = yes
 OS_DETECTION_ENABLE = yes
 SEND_STRING_ENABLE = yes
 COMBO_ENABLE = yes
 AUTO_SHIFT_ENABLE = yes
+AUTOCORRECT_ENABLE = yes
+
+BOOTMAGIC_ENABLE = no       # Bootmagic Lite
+
+# Disables
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = no            # N-Key Rollover
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+
