afc9026c7850312a82fc17ef1b9f668969c48f91
takiyu <takiyu1025.txu.development@gmail.com>
2023-07-31 15:25:56 +0000

    Arrange

--- keyboards/handwired/takiyu/config.h
+++ keyboards/handwired/takiyu/config.h
@@ -6,6 +6,13 @@
 // Make it easier for fast typists to use dual-function keys
 #define PERMISSIVE_HOLD
 
+// Disable mechanical locking support
+#undef LOCKING_SUPPORT_ENABLE
+#undef LOCKING_RESYNC_ENABLE
+
+// Limit layer up to 8
+#define LAYER_STATE_8BIT
+
 // Mouse
 #define MOUSEKEY_INTERVAL    20
 #define MOUSEKEY_DELAY       0
@@ -17,10 +24,3 @@
 #define AUTO_SHIFT_TIMEOUT 200
 #define NO_AUTO_SHIFT_ALPHA
 #define NO_AUTO_SHIFT_SPECIAL
-
-// Disable mechanical locking support
-#undef LOCKING_SUPPORT_ENABLE
-#undef LOCKING_RESYNC_ENABLE
-
-// Limit layer up to 8
-#define LAYER_STATE_8BIT
