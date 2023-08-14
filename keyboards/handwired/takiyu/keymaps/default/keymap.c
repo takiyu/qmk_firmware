45f360cdeba1ebd2f06beedebab1ecb5a5051833
takiyu <takiyu1025.txu.development@gmail.com>
2023-08-14 13:54:52 +0000

    Introduce once shift

--- keyboards/handwired/takiyu/keymaps/default/keymap.c
+++ keyboards/handwired/takiyu/keymaps/default/keymap.c
@@ -33,6 +33,7 @@ enum custom_layer {
 #define TK_ALL   LCTL(KC_A)
 #define TK_UNDO  LCTL(KC_Z)
 #define TK_WIN_E LWIN(KC_E)
+#define ONCE_SFT OSM(MOD_MASK_SHIFT)
 
 
 // -----------------------------------------------------------------------------
@@ -44,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   JP_ZKHK, KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_6   ,    KC_7   , KC_8   , KC_9   , KC_0   , JP_MINS, JP_CIRC, JP_YEN , KC_BSPC,
         TO(_HOM), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,___NG___,    KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , JP_AT  , JP_LBRC, KC_ENT ,
         MO(_1FN), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,___NG___,    KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, JP_COLN, JP_RBRC,___NG___,
-        MO(_2MO), KC_LSFT, KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,    KC_B   , KC_N   , KC_M   , JP_COMM, JP_DOT , JP_SLSH, JP_BSLS, KC_RSFT,
+        MO(_2MO),ONCE_SFT, KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,    KC_B   , KC_N   , KC_M   , JP_COMM, JP_DOT , JP_SLSH, JP_BSLS, KC_RSFT,
         MO(_3DE), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, TK_SPC1,___NG___,    TK_SPC2, KC_ESC , KC_RALT, KC_APP , KC_RCTL, KC_LEFT,KC_RIGHT, KC_PSCR
     ),
 
