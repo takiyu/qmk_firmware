1eecf4fe798a83318c0b7eabeba6c203dbdeb217
takiyu <takiyu1025.txu.development@gmail.com>
2025-06-16 18:08:30 +0900

    Disable smart up/down

--- keyboards/handwired/takiyu/keymaps/default/keymap.c
+++ keyboards/handwired/takiyu/keymaps/default/keymap.c
@@ -24,8 +24,6 @@ enum custom_layer {
 
 #define TK_SPC1  LT(_1FN, KC_SPC)
 #define TK_SPC2  LT(_2MO, KC_SPC)
-#define TK_RAL_U  MT(MOD_RALT, KC_UP)
-#define TK_RAL_D  MT(MOD_RALT, KC_DOWN)
 #define TK_STAB  LSFT(KC_TAB)
 #define TK_PSCR  LALT(KC_PSCR)
 #define TK_ALL   LCTL(KC_A)
@@ -44,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TO(_HOM), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,___NG___,    KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , JP_AT  , JP_LBRC, KC_ENT ,
         MO(_1FN), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,___NG___,    KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, JP_COLN, JP_RBRC,___NG___,
         MO(_2MO), KC_LSFT, KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,    KC_B   , KC_N   , KC_M   , JP_COMM, JP_DOT , JP_SLSH, JP_BSLS, KC_RSFT,
-        MO(_3DE), KC_LCTL, KC_RALT, KC_LWIN, KC_LALT,TK_RAL_D, TK_SPC1,___NG___,    TK_SPC2, KC_ESC ,TK_RAL_U, KC_APP , KC_RCTL, KC_LEFT,KC_RIGHT, KC_PSCR
+        MO(_3DE), KC_LCTL, KC_RALT, KC_LWIN, KC_LALT,KC_RALT, TK_SPC1,___NG___,    TK_SPC2, KC_ESC , KC_RALT, KC_APP , KC_RCTL, KC_LEFT,KC_RIGHT, KC_PSCR
     ),
 
 
