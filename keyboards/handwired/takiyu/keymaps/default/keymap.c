e86d8486699509aba88d6b1c32cc73f070e532bf
takiyu <takiyu1025.txu.development@gmail.com>
2025-06-16 16:23:54 +0900

    Update keybinds

--- keyboards/handwired/takiyu/keymaps/default/keymap.c
+++ keyboards/handwired/takiyu/keymaps/default/keymap.c
@@ -29,11 +29,12 @@ enum custom_layer {
 #define TK_TAB2  LSFT(LCTL(KC_TAB))
 #define TK_STAB  LSFT(KC_TAB)
 #define TK_PSCR  LALT(KC_PSCR)
-#define TK_CUT   LCTL(KC_X)
-#define TK_COPY  LCTL(KC_C)
-#define TK_PAST  LCTL(KC_V)
+#define TK_CUT   KC_CUT    // LCTL(KC_X)
+#define TK_COPY  KC_COPY   // LCTL(KC_C)
+#define TK_PAST  KC_PASTE  // LCTL(KC_V)
 #define TK_ALL   LCTL(KC_A)
-#define TK_UNDO  LCTL(KC_Z)
+#define TK_UNDO  KC_UNDO   // LCTL(KC_Z)
+#define TK_REDO  KC_AGAIN  // LSFT(LCTL(KC_Z))
 #define TK_WIN_E LWIN(KC_E)
 
 
@@ -47,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         TO(_HOM), KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,___NG___,    KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , JP_AT  , JP_LBRC, KC_ENT ,
         MO(_1FN), KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,___NG___,    KC_H   , KC_J   , KC_K   , KC_L   , JP_SCLN, JP_COLN, JP_RBRC,___NG___,
         MO(_2MO), KC_LSFT, KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,    KC_B   , KC_N   , KC_M   , JP_COMM, JP_DOT , JP_SLSH, JP_BSLS, KC_RSFT,
-        MO(_3DE), KC_LCTL, KC_CAPS, KC_LWIN, KC_LALT, KC_RALT, TK_SPC1,___NG___,    TK_SPC2, TK_ESC , KC_RALT, KC_APP , KC_RCTL, KC_LEFT,KC_RIGHT, KC_PSCR
+        MO(_3DE), KC_LCTL, _______, KC_LWIN, KC_LALT, KC_RALT, TK_SPC1,___NG___,    TK_SPC2, TK_ESC , KC_RALT, KC_APP , KC_RCTL, KC_LEFT,KC_RIGHT, KC_PSCR
     ),
 
 
@@ -56,16 +57,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,   _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  ,    KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 , XXXXXXX, KC_DEL ,
         TO(_HOM), TK_STAB, _______, _______,TK_WIN_E, KC_LWIN, _______,___NG___,    _______, _______, _______, _______, _______, _______, KC_PGUP, _______,
         MO(_1FN), _______, TK_ALL , _______, KC_DEL , _______, _______,___NG___,    KC_LEFT, KC_DOWN, KC_UP  ,KC_RIGHT, _______, _______, KC_PGDN,___NG___,
-        MO(_2MO), _______, _______, TK_UNDO, TK_CUT , TK_COPY, TK_PAST, _______,    KC_BSPC, _______, _______, _______, _______, _______, _______, _______,
+        MO(_2MO), TK_REDO, _______, TK_UNDO, TK_CUT , TK_COPY, TK_PAST, _______,    KC_BSPC, _______, _______, _______, _______, _______, _______, _______,
         MO(_3DE), _______, KC_SCRL, _______, _______, _______, TK_SPC1,___NG___,   DF(_1FN),DF(_HOM), _______, _______, _______,  VOL_DN,  VOL_UP, TK_PSCR
     ),
 
     // Mouse Layer
     [_2MO] = LAYOUT_5x8(
         KC_ESC,   _______, KC_F1  , TK_TAB2, TK_TAB3, KC_F4  , KC_F5  , KC_F6  ,    KC_F7  , KC_F8  , KC_HOME, KC_END ,  KC_F11,  KC_F12, XXXXXXX, KC_DEL ,
-        TO(_HOM), _______, DM_REC1, _______, _______, KC_WFWD, _______,___NG___,    _______, _______, _______, _______, _______, DM_PLY1, KC_WH_U, _______,
+        TO(_HOM), TK_STAB, DM_REC1, _______, _______, KC_WFWD, _______,___NG___,    _______, _______, _______, _______, _______, DM_PLY1, KC_WH_U, _______,
         MO(_1FN), _______, DM_RSTP, KC_BTN2, KC_BTN3, KC_BTN1, _______,___NG___,    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, JP_YEN , _______, KC_WH_D,___NG___,
-        MO(_2MO), _______, _______, _______, TK_CUT , TK_COPY, TK_PAST, _______,    KC_BSPC, _______, _______, _______, _______, _______, _______, _______,
+        MO(_2MO), TK_UNDO, _______, TK_REDO, TK_CUT , TK_COPY, TK_PAST, _______,    KC_BSPC, _______, _______, _______, _______, _______, _______, _______,
         MO(_3DE), _______, KC_SCRL, _______, _______, _______,DF(_2MO),___NG___,    TK_SPC2,DF(_HOM), _______, _______, _______,  VOL_DN,  VOL_UP, TK_PSCR
     ),
 
